432f849a581e00cabd9bacbaee3f4beeb54d26ea
Travel Planner User <user@hkwl.com>
2026-01-22 12:35:05 +0800

    fix: message schema readBy definition

--- api/models/Message.js
+++ api/models/Message.js
@@ -5,7 +5,7 @@ const messageSchema = new mongoose.Schema({
   receiver: { type: String, required: true }, // username or 'all_users' or 'admin'
   content: { type: String, required: true },
   timestamp: { type: Date, default: Date.now },
-  readBy: [{ type: String }], // Array of usernames who have read this message
+  readBy: [String], // Array of usernames who have read this message
 });
 
 module.exports = mongoose.model('Message', messageSchema);
